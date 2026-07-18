aba7a03ff01d87b7aa3ca9e098d3b24680624d73
Maylon Fernandes da Silva <132622030+maylon-fernandes@users.noreply.github.com>
2026-07-18 01:17:14 -0300

    Delete Online/teste.ps1

--- Online/teste.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-
-$form = New-Object Windows.Forms.Form
-$form.Text = "Teste Santa Casa"
-$form.Size = New-Object Drawing.Size(400,300)
-
-$form.ShowDialog()
