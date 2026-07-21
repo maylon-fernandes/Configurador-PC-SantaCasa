56aac1e4a92a24c8a38d55a6ff63068586f4707a
Maylon Fernandes da Silva <132622030+maylon-fernandes@users.noreply.github.com>
2026-07-21 14:27:29 -0300

    corrigindo cor do background

--- Online/Configurar.ps1
+++ Online/Configurar.ps1
@@ -103,7 +103,7 @@ $form.Size = New-Object Drawing.Size(850,650)
 $form.StartPosition = "CenterScreen"
 $form.FormBorderStyle = "FixedSingle"
 $form.MaximizeBox = $false
-$form.BackColor = [Drawing.Color]::MidnightBlue
+$form.BackColor = [Drawing.Color]::FromHtml("#011c45")
 
 #=================================================
 # LOGO
